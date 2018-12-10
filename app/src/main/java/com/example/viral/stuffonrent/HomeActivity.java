ce7fd4f4a6f794a8e5e587627a67b40dcb82286f
Viral <jigar21192@gmail.com>
2018-12-10 15:15:30 +0530

    just

--- app/src/main/java/com/example/viral/stuffonrent/HomeActivity.java
+++ app/src/main/java/com/example/viral/stuffonrent/HomeActivity.java
@@ -1,15 +1,21 @@
 package com.example.viral.stuffonrent;
 
 import android.support.design.widget.TabLayout;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class HomeActivity extends AppCompatActivity {
 
     TabLayout tabLayout;
     ViewPager viewPager;
     ViewPager viewPagerImage;
+    LinearLayout pagerDots;
+    private int dotscount;
+    private ImageView[] dots;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,16 +25,79 @@ public class HomeActivity extends AppCompatActivity {
         tabLayout=findViewById(R.id.tabLayout);
         viewPager=findViewById(R.id.viewPager);
         viewPagerImage=findViewById(R.id.viewpagerimage);
+        pagerDots=findViewById(R.id.pager_dots);
+
+
+        ViewPagerAdapter adapterView = new ViewPagerAdapter(this);
+        viewPagerImage.setAdapter(adapterView);
+
+        viewPagerImage.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                for(int i = 0; i< dotscount; i++){
+                    dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.unselected_dot));
+                }
+
+                dots[position].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.selected_dot));
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        dotscount = adapterView.getCount();
+        dots = new ImageView[dotscount];
+
+        for(int i = 0; i < dotscount; i++){
+
+            dots[i] = new ImageView(this);
+            dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.unselected_dot));
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+            params.setMargins(8, 0, 8, 0);
+
+            pagerDots.addView(dots[i], params);
+
+        }
+
+        dots[0].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.selected_dot));
+
+
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
 
         tabLayout.addTab(tabLayout.newTab().setText("Packages"));
         tabLayout.addTab(tabLayout.newTab().setText("Furnitures"));
         tabLayout.addTab(tabLayout.newTab().setText("Appliances"));
         tabLayout.addTab(tabLayout.newTab().setText("Vehicles"));
         tabLayout.addTab(tabLayout.newTab().setText("Costume"));
-       // tabLayout.setTabGravity(TabLayout.GRAVITY_CENTER);
 
-        ImageAdapter adapterView = new ImageAdapter(this);
-        viewPagerImage.setAdapter(adapterView);
 
         final MyAdapter adapter = new MyAdapter(this,getSupportFragmentManager(), tabLayout.getTabCount());
         viewPager.setAdapter(adapter);
@@ -51,6 +120,8 @@ public class HomeActivity extends AppCompatActivity {
 
             }
         });
+
+        // tabLayout.setTabGravity(TabLayout.GRAVITY_CENTER);
 
     }
 }
